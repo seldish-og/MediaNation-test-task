bdcfc84b4dd7f02d2b55a0a5d0cc1ad8854037ca
seldish-og <jegorbasya@mail.ru>
2022-11-23 11:11:07 +0300

    no range in main

--- app/main.py
+++ app/main.py
@@ -8,17 +8,33 @@ parser = Parser()
 file = File()
 index = Index()
 
-urls = file.get_urls()
-last_index = int(index.get_last_index())
 
-for url in urls[last_index:]:
-    url_index = urls.index(url)
-    first_thread = urls.pop(url_index)
-    second_thread = urls.pop(url_index)
+def make_result(url):
+    creds, html = parser.get_content(url=url)
+    file.write_results(url=url, creds=creds, html=html)
 
-    index.update_index(2)
 
-index.reset_redis()
+def make_threads(first_url, second_url):
+    thread1 = threading.Thread(target=make_result, args=(first_url,))
+    thread2 = threading.Thread(target=make_result, args=(second_url,))
+
+    thread1.start()
+    thread2.start()
+
+
+def main():
+    urls = file.get_urls()
+    last_index = int(index.get_last_index())
+
+    for url in urls[last_index:]:
+        url_index = urls.index(url)
+
+        first_url = urls.pop(url_index)
+        second_url = urls.pop(url_index + 1)
+        make_threads(first_url, second_url)
+        index.update_index(2)
+
+    index.reset_redis()
 
 # results = parser.get_content(url)
 # data.write_results(url=url, creds=results[0], html=results[1])
@@ -35,5 +51,5 @@ index.reset_redis()
 #     thread2.join()
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
--- app/trash/test.py
+++ app/trash/test.py
@@ -1,4 +1,13 @@
 x = ["one", "two", "three", "four"]
 
-for i in x[x.index("two")::]:
-    print(i)
+for i in range(0, len(x) - 1, 2):
+    print(x[i])
+    print(x[i + 1])
+
+
+for i in range(last_index, urls, 2):
+
+    first_url = urls[i]
+    second_url = urls[i + 1]
+    make_threads(first_url, second_url)
+    index.update_index(2)
